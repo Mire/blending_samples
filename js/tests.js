7104f203420b08882d85b88c66150603a8971274
Mirela Budaes <mbudaes@adobe.com>
2013-04-08 18:02:33 +0300

    Update background blending samples

--- js/tests.js
+++ js/tests.js
@@ -5,11 +5,9 @@ $(document).ready(function() {
 			if (event && event.target && event.target.attributes && event.target.attributes.href) {
 				var leftHtml = $(event.target).attr("href");
 				$("#test-container").attr("src", leftHtml);
-				var ref = leftHtml.replace(".html", ".png")
-				// $("#ref-container").attr("src", ref);
-				// $("#ref-container").contents("");
-				
-				$('#ref-container').contents().find('html').html("<html><body><img src='" + ref + "' style='width: 440px'/></body></html>");
+				var rightHtml = leftHtml.replace(".html", "-screenshot.html")
+				$("#ref-container").attr("src", rightHtml);
+
 			}
 		}
 	);
